bf97dc0f92845bd3fa68acfe9062d1ffbf122a83
mrzltao@126.com <mrzltao@126.com>
2021-12-17 09:13:15 +0800

    springboot访问静态资源

---
+++ learn-statics/src/main/java/learn/statics/learnstatics/LearnStaticsApplication.java
@@ -0,0 +1,13 @@
+package learn.statics.learnstatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnStaticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnStaticsApplication.class, args);
+    }
+
+}
---
+++ learn-statics/src/test/java/learn/statics/learnstatics/LearnStaticsApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.statics.learnstatics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnStaticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
